eb2671ba5613cc820e5915073b37d4fc0289d94d
Bijay Raj Paudel <bijaybzzay@gmail.com>
2021-02-19 00:54:38 -0600

    added api

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const multer = require('multer');
 
-const app = express()
+const app = express();
 app.use(cors());
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+// const upload = multer({ dest: 'uploads/' });
 
-const upload = multer({ dest: 'uploads/' })
+app.get('/', (req, res) => res.send('SIU VOG API WORKS!'));
 
-app.get('/', (req, res) => res.send('SIU VOG API WORKS!'))
-app.listen(3000, () => console.log('Server ready'))
-app.post('/update',upload.single('uploaded_file'), (req,res)=>{
+const port = process.env.PORT || 3000;
+
+app.listen(3000, () => console.log('Server ready'));
+
+const storage = multer.diskStorage(
+   {
+       destination: './uploads/',
+       filename: function ( req, file, cb ) {
+           //req.body is empty...
+           cb( null, 'audio'+ '-' + Date.now()+".wav");
+       }
+   }
+);
+
+const upload = multer( { storage: storage } );
+
+
+app.post('/update',upload.single('file'), (req,res)=>{
    console.log(req.body);
    console.log(req.file);
    res.send('{"message": "file saved"}');
